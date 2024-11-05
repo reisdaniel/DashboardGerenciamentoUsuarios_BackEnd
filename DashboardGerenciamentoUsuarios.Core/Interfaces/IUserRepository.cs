0297adb40f1e66bedf85f26adfc12b1de3506282
Daniel <danielmedeiros.dev@outlook.com>
2024-11-05 07:48:25 -0300

    Implementar interface IUserRepository e repositório UserRepository

---
+++ DashboardGerenciamentoUsuarios.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using DashboardGerenciamentoUsuarios.Core.Entities;
+
+namespace DashboardGerenciamentoUsuarios.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByEmailAsync(string email);
+        Task AddUserAsync(User user);
+
+    }
+}
