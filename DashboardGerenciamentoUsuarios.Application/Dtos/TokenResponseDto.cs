3c1440490f0d6525181c23a1f1b9a10d22302365
Daniel <danielmedeiros.dev@outlook.com>
2024-11-05 08:27:55 -0300

    Criar DTOs UserDto, LoginDto e TokenResponseDto

---
+++ DashboardGerenciamentoUsuarios.Application/Dtos/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DashboardGerenciamentoUsuarios.Application.Dtos
+{
+    public class TokenResponseDto
+    {
+        public string Token {  get; set; }
+        public DateTime Expiration {  get; set; }
+    }
+}
